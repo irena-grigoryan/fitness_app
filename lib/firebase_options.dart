e02fd6d861c2ac4d28a1807f83cd309f2b8ced3d
Irena Grigorian <irena.g@app3null.com>
2023-07-01 21:16:05 +0400

    firebase configuration for android

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -24,10 +24,7 @@ class DefaultFirebaseOptions {
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for android - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return android;
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
@@ -52,14 +49,24 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDIsW2h86mIrfornh7Y7Bwqke1Sm5PJ6jQ',
+    appId: '1:1090088400371:android:1ed6a240111697ea5ece86',
+    messagingSenderId: '1090088400371',
+    projectId: 'fitness-app-531de',
+    storageBucket: 'fitness-app-531de.appspot.com',
+  );
+
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyCmdVDH3j3-JYts12HlAq_FlyAHCjWL76I',
     appId: '1:1090088400371:ios:9e9f72dc82de43795ece86',
     messagingSenderId: '1090088400371',
     projectId: 'fitness-app-531de',
-    databaseURL: 'https://fitness-app-531de-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL:
+        'https://fitness-app-531de-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'fitness-app-531de.appspot.com',
-    iosClientId: '1090088400371-57qs76nkvnro3ed18oa6p43c8op5ngdq.apps.googleusercontent.com',
+    iosClientId:
+        '1090088400371-57qs76nkvnro3ed18oa6p43c8op5ngdq.apps.googleusercontent.com',
     iosBundleId: 'com.example.fitnessapp1',
   );
 }
